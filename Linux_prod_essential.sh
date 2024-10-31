3e32cd74a5a1ae62c3c6f7591ac367f77e01b45f
momentomori101 <110846047+momentomori101@users.noreply.github.com>
2024-10-31 15:26:05 +0530

    Update Linux_prod_essential.sh

--- Linux_prod_essential.sh
+++ Linux_prod_essential.sh
@@ -13,7 +13,8 @@ tar -czf "$backup_dir/backup_$(date
  
 #2.	System Monitoring Script: 
  
-#!/bin/bash threshold=90 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1) if [ "$cpu_usage" -gt "$threshold" ]; then     echo "High CPU usage detected: 
+#!/bin/bash threshold=90 
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1) if [ "$cpu_usage" -gt "$threshold" ]; then     echo "High CPU usage detected: 
 $cpu_usage%" 
 # Add alert/notification logic here fi 
  
